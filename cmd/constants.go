82208ad05f432d537a0528722da1a211fd6504f2
knbr13 <abdullahalaadine63@gmail.com>
2024-06-30 03:45:03 +0300

    define commands and flags names as constants

---
+++ cmd/constants.go
@@ -0,0 +1,15 @@
+package cmd
+
+const (
+	DECODE_COMMAND = "decode"
+	ENCODE_COMMAND = "encode"
+)
+
+const (
+	FILE_FLAG     = "file"
+	PASSWORD_FLAG = "password"
+	PATH_FLAG     = "path"
+	SECRET_FLAG   = "secret"
+	STDIN_FLAG    = "stdin"
+	TEXT_FLAG     = "text"
+)
